eed4966331da3236a0fa9753256a70c7216dcbe0
Buwaneka <buwaneka10000@gmail.com>
2024-12-27 18:38:27 +0530

    random indices generation 3x3

--- Front/Memory_Game/src/components/Card.jsx
+++ Front/Memory_Game/src/components/Card.jsx
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Card = () => {
-  const gridItems = new Array(9).fill(null); // Create an array with 9 elements
+  const gridItems = new Array(9).fill(null); 
+  const [active , setActive] = useState([]);
+
+  //geberate random colored boxes
+  const getRandom = () =>{
+    const random = [];
+
+    while(random.length <3){
+      const randomIndex = Math.floor(Math.random() * 9 );
+      if(!random.includes(randomIndex)){
+        random.push(randomIndex);
+      }
+    }
+    return random;
+  };
+
+  useEffect(() => {
+    
+    setActive(getRandom());
+
+    const timer = setTimeout(() => {
+      setActive([]);
+    }, 2000);
+
+    return () => {
+      clearTimeout(timer);
+    }
+
+  }, []);
 
   return (
     <div className="flex flex-col justify-center items-center h-screen w-screen">
-      <h1 className="text-3xl font-bold text-white mb-4">memorize</h1>
+      <h1 className="text-4xl font-bold text-white opacity-60 mb-10">memorize</h1>
       <div className="grid grid-cols-3 grid-rows-3 gap-2">
         {gridItems.map((_,index) => (
           <div
             key={index}
-            className="bg-green-400 h-20 w-20 rounded-md"
+            className={`bg-green-400 h-20 w-20 rounded-md 
+                        ${active.includes(index)? "bg-green-100" : "bg-green-400"}`}
           ></div>
         ))}
       </div>

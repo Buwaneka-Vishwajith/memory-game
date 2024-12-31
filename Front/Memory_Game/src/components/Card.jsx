73abf474f7973afacfecf87b68d5a7b8c19cffa6
Buwaneka <buwaneka10000@gmail.com>
2024-12-31 19:55:07 +0530

    minor changes

--- Front/Memory_Game/src/components/Card.jsx
+++ Front/Memory_Game/src/components/Card.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import Background from "./Background";
+import Background from "./Background"; //removed
 
 const MemoryGame = () => {
   const location = useLocation();
